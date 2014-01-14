6b9a44660976797cf9ca24089dd4618c4a38cb91
Rebecca Turner <turner@mikomi.org>
2014-01-14 13:00:01 -0500

    Return undef, wtf

--- lib/DBIx/Abstract.pm
+++ lib/DBIx/Abstract.pm
@@ -896,7 +896,7 @@ sub select_one_to_hashref {
     $self->__logwrite( 2, 'select_one_to_hashref' );
     $db->select(@_);
     my $result = $db->fetchrow_hashref;
-    return undef unless $result;
+    return unless $result;
     return {%$result};
 }
 
@@ -906,7 +906,7 @@ sub select_one_to_arrayref {
     $self->__logwrite( 2, 'select_one _to_arrayref' );
     $db->select(@_);
     my $result = $db->fetchrow_arrayref;
-    return undef unless $result;
+    return unless $result;
     return [@$result];
 }
 
@@ -916,7 +916,7 @@ sub select_one_to_array {
     $self->__logwrite( 2, 'select_one_to_arrayref' );
     $db->select(@_);
     my $result = $db->fetchrow_arrayref;
-    return undef unless $result;
+    return unless $result;
     return @$result;
 }
 
@@ -931,7 +931,7 @@ sub select_all_to_hashref {
     $self->__logwrite( 2, 'select_all_to_hash' );
     $db->select(@_);
     my $result = $db->fetchall_arrayref();
-    return undef unless $result;
+    return unless $result;
     my %to_ret;
     foreach (@$result) {
 
